f55dc584cab349340bc4490b65e7bd0689d72960
Emjayi <mjs32841@gmail.com>
2023-12-25 22:07:13 +0330

    contact update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Linkedin, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -23,6 +23,12 @@ const socials = [
 		label: "Github",
 		handle: "emjayi",
 	},
+	{
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/emjayi-sepahi-25114161/",
+		label: "LinkedIn",
+		handle: "Emjayi Sepahi",
+	},
 ];
 
 export default function Example() {
@@ -30,7 +36,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-300 dark:via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-4 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
 							<Link
@@ -38,10 +44,10 @@ export default function Example() {
 								target="_blank"
 								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
-								<span
+								{/* <span
 									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-200 via-zinc-200/80 dark:from-zinc-500 dark:via-zinc-500/50 to-transparent"
 									aria-hidden="true"
-								/>
+								/> */}
 								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full dark:text-zinc-200 text-zinc-800 dark:group-hover:text-white group-hover:text-black dark:group-hover:bg-zinc-900 dark:border-zinc-500 border-zinc-100 dark:bg-zinc-900 dark:group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
 								</span>{" "}
