e946aa40c378e9eac07536a82a833be71d1fc58a
Emjayi <mjs32841@gmail.com>
2024-01-07 21:22:31 +0330

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -22,7 +22,7 @@ export const Navigation: React.FC = () => {
 			<div
 				className={`fixed inset-x-0 top-0 z-50   duration-200 border-b  ${isIntersecting
 					? "bg-zinc-900/0 border-transparent"
-					: "bg-zinc-900/500  border-zinc-800 "
+					: "bg-zinc-900/500  border-zinc-800"
 					}`}
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
