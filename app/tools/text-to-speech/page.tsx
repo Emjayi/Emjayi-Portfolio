00887c4eaec8dfcad23a50f56362188dadbf6a7c
Emjayi <mjs32841@gmail.com>
2024-08-06 17:55:59 +0330

    mini projects

--- app/tools/text-to-speech/page.tsx
+++ app/tools/text-to-speech/page.tsx
@@ -1,48 +1,58 @@
-'use client';
+// 'use client';
 
-import { useState } from 'react';
-import { Textarea } from '@/components/ui/textarea';
-import { Button } from '@/components/ui/button';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import NotFound from "@/app/not-found";
 
-const TextToSpeech = () => {
-    const [text, setText] = useState<string>('');
-    const [voice, setVoice] = useState<SpeechSynthesisVoice | null>(null);
+// import { useEffect, useState } from 'react';
+// import { Textarea } from '@/components/ui/textarea';
+// import { Button } from '@/components/ui/button';
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
-    const speak = () => {
-        if (text) {
-            const utterance = new SpeechSynthesisUtterance(text);
-            if (voice) {
-                utterance.voice = voice;
-            }
-            window.speechSynthesis.speak(utterance);
-        }
-    };
+// const TextToSpeech = () => {
+//     const [text, setText] = useState<string>('');
+//     const [voice, setVoice] = useState<SpeechSynthesisVoice | null>(null);
 
-    const voices = window.speechSynthesis.getVoices();
+//     useEffect(() => {
+//         const speak = () => {
+//             if (text) {
+//                 const utterance = new SpeechSynthesisUtterance(text);
+//                 if (voice) {
+//                     utterance.voice = voice;
+//                 }
+//                 window.speechSynthesis.speak(utterance);
+//             }
+//         };
 
+//         const voices = window.speechSynthesis.getVoices();
+//     }, [text])
+
+//     return (
+//         <div className="p-6 w-full flex flex-col gap-2">
+//             <Textarea
+//                 rows={5}
+//                 placeholder="Enter Text"
+//                 value={text}
+//                 onChange={(e) => setText(e.target.value)}
+//                 className="mb-4"
+//             ></Textarea>
+//             <Select onValueChange={(value) => setVoice(voices.find((v) => v.name === value) || null)} >
+//                 <SelectTrigger>
+//                     <SelectValue placeholder="Select Voice" />
+//                 </SelectTrigger>
+//                 <SelectContent>
+//                     {voices.map((v) => (
+//                         <SelectItem key={v.name} value={v.name}>{v.name}</SelectItem>
+//                     ))}
+//                 </SelectContent>
+//             </Select>
+//             <Button onClick={speak}>Speak</Button>
+//         </div>
+//     );
+// }
+
+// export default TextToSpeech;
+
+export default function Page() {
     return (
-        <div className="p-6 w-full flex flex-col gap-2">
-            <Textarea
-                rows={5}
-                placeholder="Enter Text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                className="mb-4"
-            ></Textarea>
-            <Select onValueChange={(value) => setVoice(voices.find((v) => v.name === value) || null)} >
-                <SelectTrigger>
-                    <SelectValue placeholder="Select Voice" />
-                </SelectTrigger>
-                <SelectContent>
-                    {voices.map((v) => (
-                        <SelectItem key={v.name} value={v.name}>{v.name}</SelectItem>
-                    ))}
-                </SelectContent>
-            </Select>
-            <Button onClick={speak}>Speak</Button>
-        </div>
+        <NotFound />
     );
 }
-
-export default TextToSpeech;
